c2f12a91c5fa7825fef019986d9cc46d2a5b7fb4
Benavidez Jerónimo <jeronimo.benavidez@ingenieria.uner.edu.ar>
2024-09-05 12:20:02 -0300

    Ejercicio 1 guia 2 5/9 12:19

--- firmware/projects/Guia2_ej1/main/Guia2_ej1.c
+++ firmware/projects/Guia2_ej1/main/Guia2_ej1.c
@@ -49,74 +49,83 @@ bool midiendo= true;
 bool congelarPantalla = false;
 /*==================[internal functions declaration]=========================*/
 static void medirDistancia(void *pvParameter) {
-	printf("inicializo sensor HcSr04 /n /r" );
-	HcSr04Init(GPIO_3, GPIO_2);
-	printf("inicio ciclo while del sensor /n /r" );
-	while(true){
-		printf("mido /n /r" );
+	
+	printf("inicio ciclo while del sensor \n \r" );
+	while(1){
+	//	printf("mido \n \r" );
 		if (midiendo){
-			printf("asigno medicion a distancia /n /r");
+			//printf("asigno medicion a distancia \n \r");
 		distancia=HcSr04ReadDistanceInCentimeters();
-	}}
+		 
+	} else {};
+	vTaskDelay(1000/ portTICK_PERIOD_MS);}
 }
 
 static void leerTeclas(void *pvParameter){
-	printf("inicializo el switch /n /r");
-	SwitchesInit();
-		printf("inicio ciclo while de teclas /n /r" );
-	while(true){
-			printf("inicializo el switch /n /r" );
-	tecla  = SwitchesRead();                     
-	printf("me meto en switch de teclas /n /r" );     
-	  	switch(tecla){									
+	
+	printf("inicio ciclo while de teclas \n \r" );
+	while(1){
+	//printf("inicializo el switch \n \r" );
+	            
+	printf("me meto en switch de teclas \n \r" );     
+	  	switch(SwitchesRead()){									
     		case SWITCH_1:
-			printf("cambio variable midiendo /n /r" );
+			//printf("cambio variable midiendo \n \r" );
 			midiendo = !midiendo;
     		break;
     		case SWITCH_2:
-			printf("cambio variable congelarPantalla /n /r" );
-    		congelarPantalla = NULL;	
+			//printf("cambio variable congelarPantalla \n \r" );
+    		congelarPantalla = !congelarPantalla;	
     		break;
-			case SWITCH_1 | SWITCH_2:   			
-			printf("pongo en falso todo /n /r" );	
-			midiendo = false;
-			congelarPantalla = false;
-			break;
-}}}
+			
+}
+			vTaskDelay(100/ portTICK_PERIOD_MS);
+}}
 
 static void mostrarDistancia(void *pvParameter){
-	printf("inicializo pantalla/n /r" );
-	LcdItsE0803Init();
-	printf("inicializo leds /n /r" );
-	LedsInit();
-	printf("entro en el while de distancia /n /r" );
-	while(true){
-		printf("entro en el id de leds si midiendo = true /n /r" );
+	
+	//printf("entro en el while de distancia \n \r" );
+	while(1){
+		//printf("entro en el id de leds si midiendo = true \n \r" );
 		if(midiendo){
-		printf("entro en el if de led si distancia <10 /n /r" );	
+		//printf("entro en el if de led si distancia <10 \n \r" );	
 			if(distancia<10){
-				printf("entré en el if de led si distancia <10 /n /r" );
-				LedOff(LED_1);LedOff(LED_2);LedOff(LED_3);
+				//printf("entré en el if de led si distancia <10 \n \r" );
+				LedOff(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
 			} else if(distancia < 20){
-				printf("entré en el if de led si distancia <10/n /r" );
-					LedOn(LED_1);LedOff(LED_2);LedOff(LED_3);
+				//printf("entré en el if de led si distancia <10 \n \r" );
+					LedOn(LED_1);
+					LedOff(LED_2);
+					LedOff(LED_3);
 				} else if (distancia < 30){
-					printf("entré en el if de led si distancia <10 /n /r" );
-					LedOn(LED_1);LedOn(LED_2);LedOff(LED_3);
-					} else LedOn(LED_1);LedOn(LED_2);LedOn(LED_3);
+				//	printf("entré en el if de led si distancia <10 \n \r" );
+					LedOn(LED_1);
+					LedOn(LED_2);
+					LedOff(LED_3);
+					} else {LedOn(LED_1);LedOn(LED_2);LedOn(LED_3);}
 		}
-		printf("entro en el if del display /n /r");
+		//printf("entro en el if del display \n \r");
 		if(!congelarPantalla){
-			printf("entré en el if del display porque congelarPantalla = false /n /r" );
-			printf("inicializo LCD /n /r ");
+			//printf("entré en el if del display porque congelarPantalla = false \n \r" );
+		//	printf("inicializo LCD \n \r ");
 			LcdItsE0803Write(distancia);
 		}
-	}
+	vTaskDelay(100/ portTICK_PERIOD_MS); }
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	xTaskCreate(&medirDistancia, "midiendo distancia", 1024, NULL, 5, &medir_distancia);   
-    xTaskCreate(&leerTeclas, "leyendo teclas", 1024, NULL, 5,  &leer_teclas );  
-    xTaskCreate(&mostrarDistancia, "mostrando_distancia", 1024, NULL, 5, &mostrar_distancia);   
+	printf("inicializo sensor HcSr04 \n \r" );
+	HcSr04Init(GPIO_3, GPIO_2);
+	printf("inicializo el switch \n \r");
+	SwitchesInit();
+	printf("inicializo pantalla \n \r" );
+	LcdItsE0803Init();
+	printf("inicializo leds \n \r" );
+	LedsInit();
+	xTaskCreate(&medirDistancia, "midiendo distancia", 2048, NULL, 5, NULL);   
+    xTaskCreate(&leerTeclas, "leyendo teclas", 2048, NULL, 5,  NULL );  
+    xTaskCreate(&mostrarDistancia, "mostrando distancia", 512, NULL, 5, NULL);   
 }
 /*==================[end of file]============================================*/
