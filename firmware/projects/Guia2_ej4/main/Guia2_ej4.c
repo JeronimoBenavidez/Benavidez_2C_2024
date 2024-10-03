b94cc05105dfefca759cb3fb6cb2be3cc9116e9d
Benavidez Jerónimo <jeronimo.benavidez@ingenieria.uner.edu.ar>
2024-10-03 12:05:35 -0300

    intento de configuración del serialplotter para linux

--- firmware/projects/Guia2_ej4/main/Guia2_ej4.c
+++ firmware/projects/Guia2_ej4/main/Guia2_ej4.c
@@ -26,38 +26,51 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "analog_io_mcu.h"
 #include "uart_mcu.h"
 #include "timer_mcu.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-
 /*==================[macros and definitions]=================================*/
-uint8_t valorPote;
+uint16_t valorPote = 0;
+analog_input_config_t entradaPote;
 TaskHandle_t mandar_valores_pote_handle = NULL;
 TaskHandle_t leer_valores_pote_handle = NULL;
+float angle = 0;
 /*==================[internal data definition]===============================*/
-
+void setup()
+{
+	Serial.begin(2000);
+}
 /*==================[internal functions declaration]=========================*/
 
-void leerValoresPote (void *pvParameter)
-{
-	while(1)
-	{
-		AnalogInputReadSingle(CH1, *valorPote);
-		ulTaskNotifyTake(pdTrue , portMAX_DELAY);
-	}
-}
-
-
-
-void mandarValoresPote (void *pvParameter)
+void leerValoresPote(void *pvParameter)
 {
 	while (1)
 	{
-	UartSendByte( UART_PC , (char*)valorPote ) ;
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		AnalogInputReadSingle(CH1, &valorPote);
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		;
+	}
+}
+
+void mandarValoresPote(void *pvParameter)
+{
+	while (1)
+	{
+		UartSendString(UART_PC, "la medición es: ");
+		UartSendByte(UART_PC, (char *)UartItoa(valorPote, 10));
+		UartSendString(UART_PC, "\n");
+
+		Serial.print(">");
+
+		Serial.print("var1:");
+		Serial.print(valorPote);
+		Serial.print(",");
+
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	}
 }
 void mandar_pote(void *param)
@@ -67,12 +80,12 @@ void mandar_pote(void *param)
 
 void leer_pote(void *param)
 {
-	vTaskNotifyGiveFromISR(leer_valores_pote_handle, pdFALSE))
+	vTaskNotifyGiveFromISR(leer_valores_pote_handle, pdFALSE);
 }
 
 /*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+void app_main(void)
+{
 
 	serial_config_t my_uart = {
 		.port = UART_PC,
@@ -88,6 +101,7 @@ void app_main(void){
 		.period = 2000,
 		.func_p = mandar_pote,
 		.param_p = NULL};
+
 	TimerInit(&timer_mandar);
 
 	timer_config_t timer_leer = {
@@ -97,12 +111,11 @@ void app_main(void){
 		.param_p = NULL};
 	TimerInit(&timer_leer);
 
-		AnalogInputInit();
+	AnalogInputInit(&entradaPote);
 
-
-xTaskCreate(&leerValoresPote, "leyendo valores", 5121 NULL, 5, leer_valores_pote_handle );
-xTaskCreate(&mandarValoresPote, "midiendo valores del pote", 512, NULL, 5, mandar_valores_pote_handle) ;
-TimerStart(timer_mandar.timer);
-TimerStart(timer_leer.timer);
+	xTaskCreate(&leerValoresPote, "leyendo valores", 512, NULL, 5, leer_valores_pote_handle);
+	xTaskCreate(&mandarValoresPote, "midiendo valores del pote", 512, NULL, 5, mandar_valores_pote_handle);
+	TimerStart(timer_mandar.timer);
+	TimerStart(timer_leer.timer);
 }
 /*==================[end of file]============================================*/
